bba472d4e0ec7bd450a03102cdca6cd28c8e79f4
Yumi Smart <wesli.smart1017@gmail.com>
2023-10-17 17:52:11 -0700

    Creating function stubs for add book form

--- js/main.js
+++ js/main.js
@@ -6,3 +6,17 @@ myBook.price = 16.99;
 myBook.title = "FaeFolk";
 myBook.releaseDate = new Date(2023, 9, 17);
 console.log(myBook);
+window.onload = function () {
+    let addBookBtn = document.querySelector("#add-book");
+    addBookBtn.onclick = processBook;
+};
+function processBook() {
+    let userBook = getBook();
+    if (userBook != null) {
+        addBook(userBook);
+    }
+}
+function getBook() {
+}
+function addBook(b) {
+}
--- ts/main.ts
+++ ts/main.ts
@@ -2,10 +2,10 @@
  * represents a single book that can be purchased
  */
 class Book {
-    isbn: String;
-    title: String;
-    price: number;
-    releaseDate: Date;
+    isbn: String; //13 digit isbn number
+    title: String; //title of the book
+    price: number; //retail price of the book6
+    releaseDate: Date; //the publish date of the book
 }
 
 //Book object test code
@@ -16,3 +16,34 @@ myBook.price = 16.99;
 myBook.title = "FaeFolk"
 myBook.releaseDate = new Date(2023, 9, 17); //months start at index 0
 console.log(myBook);
+
+window.onload = function(){
+    //Set up button click for add book form
+    let addBookBtn = document.querySelector("#add-book") as HTMLButtonElement;
+    addBookBtn.onclick = processBook;
+}
+
+function processBook () {
+    //validate
+    let userBook = getBook();
+    if(userBook != null) {
+        addBook(userBook);
+    }
+}
+
+/**
+ * This function will retrieve all the book data from the HTML page.
+ * If all data is valid a Book object will be returned. If any data 
+ * is invalid, null will be returned
+ */
+function getBook ():Book {
+    
+}
+
+/**
+ * Adds a Book object to storage. Assumes all data is valid
+ * @param b The Book containing valid data to be added
+ */
+function addBook (b:Book): void {
+
+}
