e34580fbd2adc514db902b56bd01a94d47a711d7
Yumi Smart <wesli.smart1017@gmail.com>
2023-10-24 13:04:28 -0700

    hid test code

--- ts/main.ts
+++ ts/main.ts
@@ -10,12 +10,12 @@ class Book {
 
 //Book object test code
 
-let myBook = new Book();
+/*let myBook = new Book();
 myBook.isbn = "34567810";
 myBook.price = 16.99;
 myBook.title = "FaeFolk"
 myBook.releaseDate = new Date(2023, 9, 17); //months start at index 0
-console.log(myBook);
+console.log(myBook);*/
 
 window.onload = function(){
     //Set up button click for add book form
