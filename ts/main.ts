f8ab8a2b70e2c65e5c5ee9006ac492fbb52df0f1
Yumi Smart <wesli.smart1017@gmail.com>
2023-11-09 13:20:55 -0800

    Refactored addBookToStorage to store multiple books via the else statement

--- ts/main.ts
+++ ts/main.ts
@@ -156,6 +156,12 @@ function addBookToStorage (b:Book): void {
     else {
         //Parse string into a list of books and add new book to the list
         //store the newly modified list back in storage
+        let books:Book[] = JSON.parse(bookData);
+        books.push(b);
+
+        //Add back ot local storage
+        bookData = JSON.stringify(books);
+        localStorage.setItem(BookStorageKey, bookData);
     }
 }
 
